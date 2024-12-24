54e79c1b2cd99f16ffe87908581841ade1233186
DiyorbekTursunov <diyorbektursunov570@gmail.com>
2024-12-24 17:04:23 +0500

    change Auth inputs UI to new desing

--- app/components/InputFeald.tsx
+++ app/components/InputFeald.tsx
@@ -33,7 +33,7 @@ const InputField: React.FC<InputFieldProps> = ({
     }
 
     const inputClass = clsx(
-        'gap-2.5 self-stretch px-4 py-5 w-full text-sm whitespace-nowrap rounded-lg border border-solid bg-zinc-900 border-zinc-800 min-h-[56px] focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500',
+        'flex flex-col px-4 py-2 gap-1 w-full text-sm whitespace-nowrap rounded-lg bg-[#131313] ',
         {
             'focus:border-[#FF453A] focus:ring-1 focus:ring-[#FF453A]': error, // Apply red color when error exists
         },
@@ -41,47 +41,49 @@ const InputField: React.FC<InputFieldProps> = ({
 
     return (
         <div className="mt-2.5 w-full relative">
-            <label
-                htmlFor={`input-${placeholder.toLowerCase()}`}
-                className="sr-only"
-            >
-                {placeholder}
-            </label>
-            <input
-                id={`input-${placeholder.toLowerCase()}`}
-                type={type}
-                placeholder={placeholder}
-                required
-                value={inputValue}
-                autoComplete="new-password" // Add autocomplete attribute
-                className={inputClass}
-                onChange={handleInputChange} // Handle change and call parent's onChange
-            />
+            <div className={inputClass}>
+                <label
+                    htmlFor={`input-${placeholder.toLowerCase()}`}
+                    className="text-[#757575] text-xs"
+                >
+                    {placeholder}
+                </label>
+                <input
+                    id={`input-${placeholder.toLowerCase()}`}
+                    type={type}
+                    placeholder={placeholder}
+                    required
+                    value={inputValue}
+                    autoComplete="new-password" // Add autocomplete attribute
+                    className="h-full bg-transparent focus:outline-none"
+                    onChange={handleInputChange} // Handle change and call parent's onChange
+                />
 
-            {inputValue && (
-                <div className="absolute flex items-center h-full right-4 top-0 bottom-0">
-                    <button
-                        className="border-l border-white/10 pl-4 h-[30px] default-hover-active"
-                        type="button"
-                        onClick={handleClearClick} // Clear the input when clicked
-                    >
-                        <svg
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
+                {inputValue && (
+                    <div className="absolute flex items-center h-full right-4 top-0 bottom-0">
+                        <button
+                            className="border-l border-white/10 pl-4 h-[30px] default-hover-active"
+                            type="button"
+                            onClick={handleClearClick} // Clear the input when clicked
                         >
-                            <path
-                                d="M19 19L5 5M19 5L5 19"
-                                stroke="white"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                            />
-                        </svg>
-                    </button>
-                </div>
-            )}
+                            <svg
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M19 19L5 5M19 5L5 19"
+                                    stroke="white"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
