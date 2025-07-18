531350297face04273181f4e05981bdde4bebd99
Sahil Kumar <sahilbraiko70@gmail.com>
2025-07-18 08:36:43 +0530

    `Added conditional rendering of BottomMenuBar based on current pathname`

--- src/shared/Providers/UIProvider.tsx
+++ src/shared/Providers/UIProvider.tsx
@@ -3,13 +3,29 @@
 import BottomMenuBar from '@/src/shared/ui/BottomMenuBar/BottomMenuBar'
 import { PropsWithChildren } from 'react'
 import RedirectIfLandlord from '../utils/RedirectIfLandlord'
+import { usePathname } from 'next/navigation'
 
 export default function UIProvider({ children }: PropsWithChildren) {
+    const pathname = usePathname()
+
+    // Define all routes BottomMenuBar to appear
+    const bottomBarVisibleRoutes = [
+        '/',
+        '/landlord/dashboard',
+        '/messages',
+        '/landlord/chat/chat-list',
+        '/favorites',
+        '/my-objects',
+        '/profile',
+    ]
+
+    const showBottomMenu = bottomBarVisibleRoutes.includes(pathname)
+
     return (
         <div className="mx-auto w-full max-w-[480px] px-3 pt-5 mb-20">
-                        <RedirectIfLandlord />
+            <RedirectIfLandlord />
             {children}
-            <BottomMenuBar />
+            {showBottomMenu && <BottomMenuBar />}
         </div>
     )
 }
